50cd297ada4bd65d8fd447cb4073d163d71e4542
Dilshad <hmydilshadkt@gmail.com>
2024-07-13 12:46:05 +0530

    change:token verify middleware

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -10,13 +10,14 @@ export const verifyToken = (
   res: Response,
   next: NextFunction,
 ) => {
-  const token = req.cookies.token;
+  // const token = req.cookies.token;
+  const token = req.header('X-auth-token');
   if (!token)
     return res
       .status(401)
       .json({ status: false, message: 'Not authenticated' });
   jwt.verify(
-    token,
+    token.split(' ')[1],
     process.env.JWT_SECRET as string,
     async (err: jwt.VerifyErrors | null, palyload: JwtPayload | any) => {
       if (err) return res.status(403).json({ message: 'Token is not valid' });
